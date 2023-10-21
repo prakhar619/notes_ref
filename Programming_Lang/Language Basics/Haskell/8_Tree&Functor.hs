a0c1c629f903517cdec861d6cca720066417671e
Prakhar Gupta <prakhar619lm10@gmail.com>
2023-10-21 09:43:48 +0530

    Coding just Started

--- Programming_Lang/Language Basics/Haskell/8_Tree&Functor.hs
+++ Programming_Lang/Language Basics/Haskell/8_Tree&Functor.hs
@@ -7,7 +7,8 @@ instance Show a => Show (Tree a) where
  show (LeafT a) = "Leaf " ++ show a
 
 data BinaryTree a = NULL | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
---example: (Node 1 (Node 2 (Node 3 (Node 4 NULL NULL) NULL) (Node 6 NULL NULL) )(Node 8 NULL NULL))
+--example: (Node 5 (Node 3 (Node 2 NULL NULL) (Node 4 NULL NULL)) (Node 7 (Node 6 NULL NULL) (Node 8 NULL NULL)))
+        -- (Node 5 (Node 3 (Node 2 NULL NULL) (Node 4 NULL NULL)) (Node 8 (Node 6 NULL (Node 7 NULL NULL)) (Node 9 NULL NULL)))
 
 
 --takes concrete binary tree
@@ -23,7 +24,23 @@ insert x (Node v tree1 tree2) | x <= v = Node v (insert x tree1) tree2
                               | x > v = Node v tree1 (insert x tree2)
 insert x NULL = Node x NULL NULL
 
-{--
+leftmost::(BinaryTree a)->a
+leftmost (Node v NULL _ ) = v
+leftmost (Node v bt1 _ ) = leftmost bt1 
+
+successor::(Ord a)=>(BinaryTree a)->a
+successor (Node v bt1 (Node s NULL bt2 )) = s 
+successor (Node v bt1 (Node s_ bt2 bt3 )) = leftmost bt2
+
 delete::(Ord a)=>a->(BinaryTree a)->(BinaryTree a)
-delete
---}
+delete x (Node v NULL NULL)= if(x == v) then NULL else (Node v NULL NULL)
+delete x (Node v bt NULL) | x == v =  bt
+                          | x < v = Node v (delete x bt) NULL
+                          | x > v = Node v bt NULL
+delete x (Node v NULL bt) | x == v =  bt
+                          | x > v = Node v NULL (delete x bt)
+                          | x < v = Node v NULL bt
+delete x (Node v bt1 bt2) | x == v = Node suc bt1 (delete suc bt2) 
+                          | x > v = Node v bt1 (delete x bt2)
+                          | x < v = Node v (delete x bt1) bt2
+                           where suc = successor (Node v bt1 bt2)
